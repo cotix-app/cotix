b5e2a26b3fbeda508f0e7431a7931f0217e03b8b
Ezequiel Ruiz <ruizvillamaria@gmail.com>
2026-03-04 17:52:53 -0300

    add icon splash y favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Cotix",
+        short_name: "Cotix",
+        description: "Cotix - Presupuestos técnicos rápidos",
+        theme_color: "#1d4ed8",
+        background_color: "#1d4ed8",
+        display: "standalone",
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
